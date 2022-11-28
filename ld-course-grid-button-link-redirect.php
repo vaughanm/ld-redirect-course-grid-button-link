0ca90fa291780f4bddf132bff0f99e802e717c28
Vaughan Montgomery <vaughan.montgomery@gmail.com>
2022-11-28 16:46:37 +0000

    Update ld-course-grid-button-link-redirect.php
    Fixed issue for checking if user is already enrolled in that course.

--- ld-course-grid-button-link-redirect.php
+++ ld-course-grid-button-link-redirect.php
@@ -3,8 +3,10 @@
 add_filter( 'learndash_course_grid_custom_button_link', function($button_link, $post_id ) {
     $ld_registration_page_id = (int) LearnDash_Settings_Section::get_section_setting( 'LearnDash_Settings_Section_Registration_Pages', 'registration' );
     if( ! empty( $ld_registration_page_id ) ) {
-	$register_url = get_permalink( $ld_registration_page_id );
-        $button_link = $register_url . '?ld_register_id=' . $post_id; 
+        if( ( is_user_logged_in() && !sfwd_lms_has_access( $post_id, get_current_user_id() ) ) || !is_user_logged_in() ) {
+            $register_url = get_permalink( $ld_registration_page_id );
+            $button_link = $register_url . '?ld_register_id=' . $post_id;
+        }
     }
     
     return $button_link;
